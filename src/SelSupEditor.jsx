e2ccea5104194282e8b6cabe73ddf4ee23ba54b1
8Lou <lou8@mail.ru>
2024-03-06 23:31:30 +0300

    color create

--- src/SelSupEditor.jsx
+++ src/SelSupEditor.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
 
 interface Param {
   id: number;
   name: string;
-  type: 'string';
+  type: "string";
+}
+
+interface Color {
+  id: number;
+  name: string;
 }
 
 interface ParamValue {
@@ -21,32 +26,86 @@ interface Props {
   model: Model;
 }
 
+interface State {
+  model: Model;
+  selectedColorId: number | null;
+}
+
 class ParamEditor extends React.Component<Props, State> {
-  public getModel(): Model {
-    return this.state.model;
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      model: props.model,
+      selectedColorId: null,
+    };
   }
+
+  handleChange = (paramId: number, value: string) => {
+    const updatedParamValues = this.state.model.paramValues.map((paramValue) =>
+      paramValue.paramId === paramId ? { ...paramValue, value } : paramValue
+    );
+    this.setState({
+      model: { ...this.state.model, paramValues: updatedParamValues },
+    });
+  };
+
+  handleColorChange = (colorId: number) => {
+    this.setState({ selectedColorId: colorId });
+  };
+
+  getModel = (): Model => {
+    return { ...this.state.model };
+  };
 
   render() {
     const { params, model } = this.props;
+    const { selectedColorId } = this.state;
 
     return (
       <div>
         <h2>SelSup Editor</h2>
-        {params.map((param) => (
-          <div key={param.id}>
-            <label>{param.name}: </label>
-            <input
-              type="text"
-              value={model.paramValues.find((p) => p.paramId === param.id)?.value || ''}
-              onChange={(e) => this.handleChange(param.id, e.target.value)}
-            />
+        {params &&
+          params.map((param) => (
+            <div key={param.id}>
+              <label>{param.name}: </label>
+              <input
+                type="text"
+                value={
+                  model.paramValues.find((p) => p.paramId === param.id)
+                    ?.value || ""
+                }
+                onChange={(e) => this.handleChange(param.id, e.target.value)}
+              />
+            </div>
+          ))}
+        {model && model.colors && (
+          <div>
+            <h3>Выберите цвет:</h3>
+            {model.colors.map((color) => (
+              <button
+                key={color.id}
+                style={{
+                  backgroundColor: color.name,
+                  color: "white",
+                  padding: "5px",
+                  margin: "5px",
+                  cursor: "pointer",
+                }}
+                onClick={() => this.handleColorChange(color.id)}
+              >
+                {color.name}
+              </button>
+            ))}
+            <p>
+              Выбранный цвет:{" "}
+              {model.colors.find((c) => c.id === selectedColorId)?.name ||
+                "Не выбрано"}
+            </p>
           </div>
         )}
       </div>
     );
   }
 }
-
-// ColorPicker компонент и определения типов данных здесь
 
 export default ParamEditor;
